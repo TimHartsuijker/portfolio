e98aed00014de0e3fae2a425eb45888e7fea73ba
Tim Hartsuijker <tim.hartsuijker@rocdeleijgraaf.nl>
2021-06-21 10:19:09 +0200

    get down with the sickness

---
+++ paginas/wiebenik.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wie ben ik?</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+<div class="bar2">
+    <a class="links" href="../index.html">terug</a>
+</div>
+<div class="text">mijn naam is Tim Hartsuijker en ik woon in Oss.</div>
+<div class="text">Ik ben 17 jaar oud en in mijn vrije tijd speel ik graag videogames zoals:</div>
+<div class="text"><a href="https://playvalorant.com/en-us/" class="links" target="_blank">Valorant</a></div>
+<div class="text"><a href="https://www.ea.com/en-gb/games/apex-legends" class="links" target="_blank">Apex Legends</a></div>
+<div class="text"><a href="https://www.minecraft.net/nl-nl" class="links" target="_blank">Minecraft</a></div>
+<div class="text"><a href="https://store.steampowered.com/app/346110/ARK_Survival_Evolved/" class="links" target="_blank">ARK: survival evolved</a></div>
+<div class="text">verder kijk ik vaak anime en schaak ik graag.</div>
+<div class="text">schaken doe ik vaak via de site: <a href="https://lichess.org/" class="links" target="_blank">lichess.org</a> </div>
+
+</body>
+</html>
